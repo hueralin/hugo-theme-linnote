42f8782b5b3d20dcd0ca561e7637e577870d1600
hueralin <1612415787@qq.com>
2022-06-28 00:47:23 +0800

    feat: layout & tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+const theme = {
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
+  colors: {
+    gray: colors.gray,
+    blue: colors.sky,
+    red: colors.rose,
+    pink: colors.fuchsia,
+  },
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
+  },
+  extend: {
+    spacing: {
+      '128': '32rem',
+      '144': '36rem',
+    },
+    borderRadius: {
+      '4xl': '2rem',
+    }
+  }
+}
+
 module.exports = {
-  purge: ['layouts/**/*.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  content: ["content/**/*.md", "layouts/**/*.html"],
+  theme,
   plugins: [],
 }
